3107411f15cb588faab16dcf40526566693db878
erp_dev_001 <fredditampubolon66@gmail.com>
2024-09-14 15:34:09 +0700

    add new model

--- Apps/demo/models.py
+++ Apps/demo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=150, null=True, blank=False, unique=True)
+    description = models.TextField(verbose_name=_('Description'), max_length=125, blank=True)
+    rating = models.DecimalField(verbose_name=_('Price'), default=0, decimal_places=2, max_digits=5)
+    published = models.DateField(verbose_name=_('Publish Date'), blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(verbose_name=_('Image'), upload_to='covers/', blank=True)
--- Backend/urls.py
+++ Backend/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Django Vue JS Admin"
+admin.site.site_title = "Django Vue JS Admin Portal"
+admin.site.index_title = "Welcome to the admin side Django Vue JS"
