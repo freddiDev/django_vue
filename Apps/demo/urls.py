69c65f85f95ca8465eb11e2543bc576fee8db5b8
erp_dev_001 <fredditampubolon66@gmail.com>
2024-09-14 16:25:31 +0700

    object method

--- Apps/demo/urls.py
+++ Apps/demo/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from .views import Index
 
 urlpatterns = [
     path('', views.index),
+    path('data/', Index.as_view()),
+
 ]
 
--- Apps/demo/views.py
+++ Apps/demo/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Book
 # Create your views here.
+
+
+class Index(View):
+    books = Book.objects.all()
+
+    #filter method
+    # select data by parameter value
+    # Book.objects.filter(parameter (is_published=True))
+
+    #get method
+    # select data, and retun single data
+    # Book.objects.get(parameter (id=2))
+
+    output = ""
+    for book in books:
+        output += f"Title book is {book.title} <br>"
+    
+    def get(self, request):
+        return HttpResponse(self.output)
+
 
 def index(request):
     return HttpResponse('From message from view')
